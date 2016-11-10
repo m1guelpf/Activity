1b5c29655733e834cc7534c108a2df0b392210d0
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-10 10:18:23 +0100

    Create client.php

---
+++ client.php
@@ -0,0 +1,13 @@
+$data = array("name" => "Hagrid", "age" => "36");                                                                    
+$data_string = json_encode($data);                                                                                   
+                                                                                                                     
+$ch = curl_init('URL');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);                                                                                                                   
+                                                                                                                     
+$result = curl_exec($ch);
