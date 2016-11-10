6b19477838fe18be7ba27251435a2f5bda143260
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-10 10:20:26 +0100

    Update client.php

--- client.php
+++ client.php
@@ -6,7 +6,8 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                                
+    'Content-Type: application/json',         
+    'Authorization: '.$token,
     'Content-Length: ' . strlen($data_string))                                                                       
 );                                                                                                                   
                                                                                                                      
