e225f36bd57a1e96b6941c838f18e021c21bc24a
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-18 17:02:56 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- client.php
+++ client.php
@@ -1,20 +1,21 @@
 <?php
-$token = "test";
-$data = array("name" => "Hagrid", "age" => "36");                                                                    
-$data_string = json_encode($data);                                                                                   
-                                                                                                                     
-$ch = curl_init('http://activity.local.dev');                                                                      
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',         
+
+$token = 'test';
+$data = ['name' => 'Hagrid', 'age' => '36'];
+$data_string = json_encode($data);
+
+$ch = curl_init('http://activity.local.dev');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
     'Authorization: '.$token,
-    'Content-Length: ' . strlen($data_string))                                                                       
-);                                                                                                                   
-                                                                                                                     
+    'Content-Length: '.strlen($data_string), ]
+);
+
 $result = curl_exec($ch);
-if ($result == null){
-echo "null";
+if ($result == null) {
+    echo 'null';
 }
 echo $result;
