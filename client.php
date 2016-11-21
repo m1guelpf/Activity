80bc2ffaa4e645cc52fc39ee2050541316fc0e87
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-21 21:52:51 +0100

    Test

--- client.php
+++ client.php
@@ -9,7 +9,7 @@ $data_string = json_encode($data);
 
 $ch = curl_init('http://activity.local.dev/client.php');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_POSTFIELDS, array("$data"=>$data_string));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: application/json',
