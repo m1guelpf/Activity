71679e70070afc353f407019ca8f08e1f55a0b4a
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-21 21:50:12 +0100

    Dev tests

--- client.php
+++ client.php
@@ -1,10 +1,13 @@
 <?php
-
+if (isset($_GET['result'])){
+echo $_GET['result'];
+exit();
+}
 $token = 'test';
 $data = ['name' => 'Hagrid', 'age' => '36'];
 $data_string = json_encode($data);
 
-$ch = curl_init('http://activity.local.dev');
+$ch = curl_init('http://activity.local.dev/client.php');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -15,7 +18,3 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 );
 
 $result = curl_exec($ch);
-if ($result === null) {
-    echo 'null';
-}
-echo $result;
