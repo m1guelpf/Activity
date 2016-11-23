0852b0cf6681688f307721cc9f57097d0aedbd77
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-23 17:52:35 +0100

    Remove tests

--- index.php
+++ index.php
@@ -35,8 +35,7 @@ $token = $mysqli->real_escape_string($_GET['token']);
         exit();
     }
     $token = $result->fetch_assoc();
-    echo json_encode($_POST);
-    exit;
+
     updateActivity($activityType, $activityTitle, $activityOrigin);
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (!isset($_GET['token']) || $_GET['token'] === null) {
