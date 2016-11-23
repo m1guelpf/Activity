8958a5fb87d7e734268e554cb966d0b7914e8afc
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-11-23 20:28:59 +0100

    Node Example

---
+++ examples/example.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+// Set the headers
+var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+}
+
+// Configure the request
+var options = {
+    url: 'http://activity.local.dev?token=test',
+    method: 'POST',
+    headers: headers,
+    form: {'activityType': '1', 'activityTitle': 'NodeJS', 'activityIP': '127.0.0.1', 'activityUserAgent': 'NodeJS'}
+}
+
+// Start the request
+request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Print out the response body
+        console.log(body)
+    }
+})
