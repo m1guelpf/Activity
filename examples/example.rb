810edc7a20f16eb6b06580fd714ff4ef0d0d91c3
Miguel Piedrafita <git@miguelpiedrafita.com>
2016-12-01 11:15:52 +0100

    Upload

--- examples/example.rb
+++
@@ -1,3 +0,0 @@
-require 'net/http'
-uri = URI.parse("http://activity.local.dev?token=test")
-Net::HTTP.post_form(uri, {"activityType" => "1", "activityTitle" => "Ruby", "activityIP" => "127.0.0.1", "activityUserAgent" => "Ruby"})
